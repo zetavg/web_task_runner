ae353a28287cafb02a4f658d0738c76bcaf9c102
Neson <neson@dex.tw>
2015-05-29 16:28:04 +0800

    fix sinatra loading error

--- lib/web_task_runner/version.rb
+++ lib/web_task_runner/version.rb
@@ -1,5 +1,9 @@
-require 'sinatra'
-
-class WebTaskRunner < Sinatra::Application
-  VERSION = "0.0.1"
+if defined? Sinatra::Application
+  class WebTaskRunner < Sinatra::Application
+    VERSION = "0.0.1"
+  end
+else
+  class WebTaskRunner
+    VERSION = "0.0.1"
+  end
 end
